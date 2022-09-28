0710861031ab35eb201c173aac556e9b5a8a6891
Lucas Alves <lucasdolohov@gmail.com>
2022-09-28 09:30:38 -0300

    feat(home-screen): :sparkles: cria o componente header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import { Logo, Row, Button } from 'components'
+
+function HeaderComponent() {
+  return (
+    <header>
+      <Row alignItems='center' justifyContent='space-between' p={15}>
+        <Logo height={40} />
+        <Button variant='primary' border={0} width={150}>
+          Sair
+        </Button>
+      </Row>
+    </header>
+  )
+}
+
+export default HeaderComponent
